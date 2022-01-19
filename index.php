5da3563af37a89497afef48b2830453341309bbf
Douglas Martinez <douglasalexandermartinez@gmail.com>
2022-01-18 22:14:08 -0600

    primer commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<title> Documento sin titulo</title>
+
+</head>
+
+<body>
+
+<?php
+
+	$nombre="Juan";
+
+	function damenombre(){
+
+		$nombre="Maria";
+
+	}
+
+	damenombre();
+
+	echo $nombre;
+
+
+
+?>
+
+
+		
+
+</body>
+</html>
+
+
